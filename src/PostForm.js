3aa812a76f40e0d5f6121c276c6c551d4d2a9dc0
Paul Ovechkin <paul.ovechkin@gmail.com>
2016-09-18 23:24:38 +0300

    Correct PostForm classname

--- src/PostForm.js
+++ src/PostForm.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 
-export default class Post extends Component {
+export default class PostForm extends Component {
   render() {
     return (
       <div>¡Lo siento! La creación del registro está prohibido.</div>
