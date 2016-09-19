6aa2d9dc62000a382baa1b03c73d918ba6d6a1ee
Paul Ovechkin <paul.ovechkin@gmail.com>
2016-09-20 02:27:53 +0300

    Introduce PostList component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import Post from './Post'
+import PostList from './PostList'
 import PostForm from './PostForm'
 
 export default class App extends Component {
@@ -7,10 +7,6 @@ export default class App extends Component {
     return (
       <main>
         <h1>Noticias</h1>
-        <Post id={1} name="Episodio 2002 “Skank Hunt” Comunicado de prensa">
-          La pesca de arrastre en Twitter continúa en un nuevo episodio
-          se estrenará Miércoles 21 de septiembre a 22:00 ET / PT en Comedy Central
-        </Post>
         <PostForm />
       </main>
     )
---
+++ src/PostList.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import Post from './Post'
+
+export default class PostList extends Component {
+  render() {
+    return (
+      <div id="postlist">
+      </div>
+    )
+  }
+}
