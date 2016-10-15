fb014d9988944e3e5e3279b3aa3d4c22491aafd9
Paul Ovechkin <paul.ovechkin@gmail.com>
2016-10-16 01:53:26 +0300

    Add data to App state

--- src/App.js
+++ src/App.js
@@ -4,28 +4,13 @@ import PostList from './PostList'
 import PostForm from './PostForm'
 
 export default class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { data: [] }
+  }
+
   render() {
-    const data = [
-      {
-        id: 1,
-        name: 'South Park consigue un canal Giphy',
-        description:
-          `Cada momento de South Park imaginables etiquetado
-          y organizado por temporada, episodio, carácter, reacción, eslogan y más.`
-      },
-      {
-        id: 2,
-        name: 'South Park devoluciones para la temporada 20 el 14 de septiembre a las 10 pm',
-        description: 'South Park devoluciones para la temporada 20'
-      },
-      {
-        id: 3,
-        name: 'Episodio 2001 “Bayas miembros” Comunicado de prensa',
-        description:
-          `South Park temporada 20 se inicia con un nuevo episodio
-          se estrenará Miércoles, 14 de septiembre a 22:00 ET / PT en Comedy Central`
-      }
-    ]
+    const { data } = this.state
 
     return (
       <main>
